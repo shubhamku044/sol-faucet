b0c96d3acee72ba701e410c556058bb193d8ab02
shubhamku044 <shubhamku044@gmail.com>
2025-01-05 11:56:46 +0530

    tailwind setup

--- src/components/airdrop.tsx
+++ src/components/airdrop.tsx
@@ -1,24 +1,22 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 
 function Airdrop() {
-  const wallet = useWallet()
-  const { connection } = useConnection()
+  const wallet = useWallet();
+  const { connection } = useConnection();
 
   const sendAirdropToUser = () => {
-    if (!wallet.publicKey) return
-    connection.requestAirdrop(wallet.publicKey, 1 * 10 ** 9)
-  }
+    if (!wallet.publicKey) return;
+    connection.requestAirdrop(wallet.publicKey, 1 * 10 ** 9);
+  };
 
   return (
     <div>
       Hello, {wallet.publicKey?.toBase58()}
       <br />
       <input type="number" max={5} min={0.1} step={0.1} placeholder="amount" />
-      <button
-        onClick={sendAirdropToUser}
-      >Request Airdrop</button>
+      <button onClick={sendAirdropToUser}>Request Airdrop</button>
     </div>
   );
 }
 
-export default Airdrop
+export default Airdrop;
